9b8a689b94e65f15a3d1a187599ddd00208f6e5e
euge_godino <142047151+euge7777@users.noreply.github.com>
2024-05-20 09:13:46 -0300

    se agregó java para conversor de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,13 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function cambiarUnidades (id,value) {
+    if (isNaN(value)) {
+        alert('Se ingresó un valor inválido');
+        document.lasUnidades.unid_metro.value = " ";
+    } else if (id=="metro") {
+        document.lasUnidades.unid_pulgada.value = 39.3701 * value;
+        document.lasUnidades.unid_pie.value = 3.28084 * value;
+        document.lasUnidades.unid_yarda.value = 1.09361 * value;
+    }
+}
