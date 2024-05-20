19c0a7ce749fe714c20168f46bd5820a252ea111
euge_godino <142047151+euge7777@users.noreply.github.com>
2024-05-20 10:06:35 -0300

    se agregó conversor de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -13,5 +13,17 @@ function cambiarUnidades (id,value) {
         document.lasUnidades.unid_pulgada.value = 39.3701 * value;
         document.lasUnidades.unid_pie.value = 3.28084 * value;
         document.lasUnidades.unid_yarda.value = 1.09361 * value;
+    } else if (id=="pulgada"){
+        document.lasUnidades.unid_metro.value = 0.0254 * value;
+        document.lasUnidades.unid_pie = 0.0833 * value;
+        document.lasUnidades.unid_yarda = 0.0277778 * value;
+    } else if (id=="pie"){
+        document.lasUnidades.unid_metro = 0.3048 * value;
+        document.lasUnidades.unid_pulgada = 12 * value;
+        document.lasUnidades.unid_yarda = 0.3333 * value;
+    } else if (id=="yarda"){
+        document.lasUnidades.unid_metro = 0.9144 * value;
+        document.lasUnidades.unid_pulgada = 36 * value;
+        document.lasUnidades.unid_pie = 3 * value;
     }
 }
