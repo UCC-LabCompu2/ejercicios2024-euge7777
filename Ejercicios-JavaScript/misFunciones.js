8e0a1c5310468d9f6fa7052f83b04ce2da83c8c4
euge_godino <142047151+euge7777@users.noreply.github.com>
2024-05-27 09:21:09 -0300

    se agregó math.round

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -3,36 +3,55 @@
  * @method cambiarUnidades
  * @param {string} id - El id de los inputs de metros, yardas, pies o pulgadas
  * @param {number} value - El valor de los inputs de metrosm yardas, pies o pulgadas
- * @return 
  */
-function cambiarUnidades (id,value) {
-    if (isNaN(value)) {
-        alert('Se ingresó un valor inválido');
-        document.lasUnidades.unid_metro.value = " ";
-    } else if (id=="metro") {
-        document.lasUnidades.unid_pulgada.value = 39.3701 * value;
-        document.lasUnidades.unid_pie.value = 3.28084 * value;
-        document.lasUnidades.unid_yarda.value = 1.09361 * value;
-    } else if (id=="pulgada"){
-        document.lasUnidades.unid_metro.value = 0.0254 * value;
-        document.lasUnidades.unid_pie = 0.0833 * value;
-        document.lasUnidades.unid_yarda = 0.0277778 * value;
-    } else if (id=="pie"){
-        document.lasUnidades.unid_metro = 0.3048 * value;
-        document.lasUnidades.unid_pulgada = 12 * value;
-        document.lasUnidades.unid_yarda = 0.3333 * value;
-    } else if (id=="yarda"){
-        document.lasUnidades.unid_metro = 0.9144 * value;
-        document.lasUnidades.unid_pulgada = 36 * value;
-        document.lasUnidades.unid_pie = 3 * value;
+let cambiarUnidades = (nombre, valor) => {
+    let valMetro, valPulgada, valPie, valYarda
+
+    if (valor.includes(",")) {
+        valor = valor.replace(",", ".")
     }
+
+    if (isNaN(valor)) {
+        alert("El valor ingresado no es un número!")
+        valMetro = ""
+        valPulgada = ""
+        valPie = ""
+        valYarda = ""
+    } else {
+        if (nombre === "metro") {
+            valMetro = valor
+            valPulgada = valor * 39.3701
+            valPie = valor * 3.28084
+            valYarda = valor * 1.093
+        } else if (nombre === "pulgada") {
+            valMetro = valor * 0.0254
+            valPulgada = valor
+            valPie = valor * 0.08333
+            valYarda = valor * 0.02777
+        } else if (nombre === "pie") {
+            valMetro = valor * 0.3048
+            valPulgada = valor * 12
+            valPie = valor
+            valYarda = valor * 0.33333
+        } else if (nombre === "yarda") {
+            valMetro = valor * 0.9144
+            valPulgada = valor * 36
+            valPie = valor * 3
+            valYarda = valor
+        }
+    }
+
+    document.getElementById('metro').value = Math.round(valMetro*100)/100
+    document.getElementById('pulgada').value = Math.round(valPulgada*100)/100
+    document.getElementById('pie').value = Math.round(valPie*100)/100
+    document.getElementById('yarda').value = Math.round(valYarda*100)/100
 }
+
 /**
  * Conversor de unidades de grados  radianes
  * @method convertirGR
  * @param {string} id - El id de los inputs de grados o radianes
  * @param {number} value - El valor de los inputs de grados o radianes
- * @return
  */
 function convertirGR (id){
     var grad, rad;
@@ -51,7 +70,6 @@ function convertirGR (id){
  * Mostrar u ocultar un div
  * @method mostrar_ocultar
  * @param {string}valorMO - el  id del div que se desea mostrar / ocultar
- * @return
  */
 function mostrar_ocultar (valorMO){
     if (valorMO=="val_mostrar"){
@@ -61,6 +79,10 @@ function mostrar_ocultar (valorMO){
     }
 }
 
+/**
+* Sumar valores ingresados por el usuario
+* @method calcularSuma
+*/
 function calcularSuma(){
     var num1, num2;
     num1=Number(document.getElementsByName("sum_num1")[0].value);
@@ -69,6 +91,10 @@ function calcularSuma(){
 
 }
 
+/**
+* Restar los valores ingresados por el usuario
+* @method calcularResta
+*/
 function calcularResta(){
     var num1, num2;
     num1=Number(document.getElementsByName("res_num1")[0].value);
@@ -76,6 +102,10 @@ function calcularResta(){
     document.getElementsByName("res_total")[0].value=num1-Number(num2);
 }
 
+/**
+* Multiplicar los valores por el usuario
+* @method calcularMultiplicacion
+*/
 function calcularMultiplicacion (){
     var num1, num2;
     num1=Number(document.getElementsByName("mul_num1")[0].value);
@@ -83,11 +113,13 @@ function calcularMultiplicacion (){
     document.getElementsByName("mul_total")[0].value=num1*Number(num2);
 }
 
+/**
+* Dividir los valores ingresados por el usuario
+* @method calcularDivision
+*/
 function calcularDivision (){
     var num1, num2;
     num1=Number(document.getElementsByName("div_num1")[0].value);
     num2=Number(document.getElementsByName("div_num2")[0].value);
     document.getElementsByName("div_total")[0].value=num1/Number(num2);
-
-
 }
